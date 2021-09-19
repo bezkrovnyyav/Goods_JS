87d23aa6d24bf3b500fef0eb41ad35c3bd655a4e
Andrii <bezkrovnyyav@gmail.com>
2021-09-19 18:37:10 +0300

    Add type validation off array

--- goods.js
+++ goods.js
@@ -9,3 +9,46 @@ let parsingProducts = (products) => {
 
 let goods = parsingProducts(products);
 
+
+let validateGoods = (products) => {
+  products.forEach((object) => {
+    if (
+      typeof object.item !== "undefined" &
+      typeof object.type !== "undefined" &
+      typeof object.weight !== "undefined" &
+      typeof object.pricePerKilo !== "undefined"
+    ) {
+      object.pricePerKilo = parseFloat(
+        object.pricePerKilo.replace("$", "".split(",").join("."))
+      );
+      if (
+        typeof object.item === "string" &
+        typeof object.type === "string" &
+        typeof object.quantity !== "undefined" &
+        typeof object.pricePerItem === "number"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (
+      typeof object.item !== "undefined" &
+      typeof object.type !== "undefined" &
+      typeof object.quantity !== "undefined" &
+      typeof object.pricePerItem !== "undefined"
+    ) {
+      object.pricePerItem = parseFloat(
+        object.pricePerItem.replace("$", "").split(",").join(".")
+      );
+      return (
+        typeof object.item === "string" &
+        typeof object.type === "string" &
+        typeof object.quantity === "number" &
+        typeof object.pricePerItem === "number"
+      );
+    }
+  });
+};
+
+validateGoods(goods);
+
